4c4b8bf9e47a300c35f64b2144fb66085ea6cb5f
William Tucker <william.tucker29@gmail.com>
2024-03-19 18:40:03 -0500

    Fixed issue with not selecting the right parameters of incoming request to pass into model. Changed SQL query. Need to look at refactoring. Seems very complex. However this does produce the correct question from my seeded data when using postman.

--- server/controllers/getQuestions.js
+++ server/controllers/getQuestions.js
@@ -2,8 +2,10 @@
 const { getQuestions } = require('../models');
 
 module.exports = async (req, res) => {
+  console.log('req.query in controllers: ', req.query);
   try {
-    const questions = await getQuestions();
+    const productId = req.query.product_id;
+    const questions = await getQuestions(productId);
     res.status(200).json(questions);
   } catch (error) {
     console.error('Error retrieving questions: ', error);
--- server/models/getQuestions.js
+++ server/models/getQuestions.js
@@ -1,30 +1,45 @@
 /* eslint-disable no-console */
 const db = require('../db/database.js');
 
-module.exports = async (req, res) => {
+module.exports = async (productId) => {
+  console.log('productId in models: ', productId);
   try {
-    const productId = req.params.product_id;
     const query = `
-      SELECT
-        q.question_id,
-        q.question_body,
-        q.question_date,
-        q.asker_name,
-        q.question_helpfulness,
-        q.reported,
-        JSON_OBJECTAGG(a.answer_id, JSON_OBJECT(
+    SELECT
+    q.question_id,
+    q.question_body,
+    q.question_date,
+    q.asker_name,
+    q.question_helpfulness,
+    q.reported,
+    JSON_OBJECT(
+      'answers', q_answers.answers
+    ) AS answers
+  FROM questions q
+  LEFT JOIN (
+    SELECT
+      a.question_id,
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
           'id', a.answer_id,
           'body', a.body,
           'date', a.date,
           'answerer_name', a.answerer_name,
           'helpfulness', a.helpfulness,
-          'photos', JSON_ARRAYAGG(p.photo_url)
-        )) AS answers
-      FROM questions q
-      LEFT JOIN answers a ON q.question_id = a.question_id
-      LEFT JOIN photos p ON a.answer_id = p.answer_id
-      WHERE q.product_id = ?
-      GROUP BY q.question_id;
+          'photos', photos.photo_urls
+        )
+      ) AS answers
+    FROM answers a
+    LEFT JOIN (
+      SELECT
+        answer_id,
+        JSON_ARRAYAGG(photo_url) AS photo_urls
+      FROM photos
+      GROUP BY answer_id
+    ) AS photos ON a.answer_id = photos.answer_id
+    GROUP BY a.question_id
+  ) AS q_answers ON q.question_id = q_answers.question_id
+  WHERE q.product_id = ?
     `;
     const [rows] = await db.promise().query(query, [productId]);
     const formattedData = rows.map((row) => ({
@@ -34,7 +49,7 @@ module.exports = async (req, res) => {
       asker_name: row.asker_name,
       question_helpfulness: row.question_helpfulness,
       reported: row.reported === 1,
-      answers: row.answers ? JSON.parse(row.answers) : {},
+      answers: typeof row.answers === 'string' ? JSON.parse(row.answers) : row.answers,
     }));
 
     const responseObject = {
@@ -42,9 +57,9 @@ module.exports = async (req, res) => {
       results: formattedData,
     };
 
-    res.json(responseObject);
+    return responseObject;
   } catch (error) {
     console.error('Error retrieving questions: ', error);
-    res.status(500).json({ error: 'Internal server error' });
+    throw error;
   }
 };
