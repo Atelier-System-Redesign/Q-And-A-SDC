deda3eed101edb104f35394ab35d538695d6796c
William Tucker <william.tucker29@gmail.com>
2024-03-19 13:24:58 -0500

    Appease linter gods.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 // Middleware
 const morgan = require('morgan');
