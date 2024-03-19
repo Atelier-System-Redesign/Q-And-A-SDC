1d99895416c06f0c8607a8a32c4f6f12abc686ec
William Tucker <william.tucker29@gmail.com>
2024-03-19 13:29:07 -0500

    Connection to SQL database

---
+++ server/db/database.js
@@ -0,0 +1,14 @@
+// connect to mysql
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect();
+
+module.exports = connection;
