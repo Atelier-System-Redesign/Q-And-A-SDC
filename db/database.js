de0e92d4e7ac0f6706ffd18827bcccd7d5ff1d48
William Tucker <william.tucker29@gmail.com>
2024-03-22 17:29:37 -0500

    Updates for deployment

--- db/database.js
+++ db/database.js
@@ -5,8 +5,8 @@ const { Client } = require('pg');
 
 function createClient() {
   return new Client({
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     host: process.env.DB_HOST,
     port: process.env.PORT,
     database: process.env.DB_NAME,
--- script.js
+++ script.js
@@ -5,9 +5,7 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
+
   stages: [
     { duration: '10s', target: 100 },
     { duration: '15s', target: 100 },
