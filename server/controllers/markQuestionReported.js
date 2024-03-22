09132bde594a5c2d3d87d3191b0cae0afa8e810c
William Tucker <william.tucker29@gmail.com>
2024-03-22 12:11:17 -0500

    Built out API for marking questions as reported. Allows for questions to be un-reported as well. Tested with Postman and then tested the GET request to ensure question removal if reported.

--- server/controllers/markQuestionReported.js
+++ server/controllers/markQuestionReported.js
@@ -1,3 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
 const { markQuestionReported } = require('../models');
 
-module.exports = (req, res) => {};
+module.exports = async (req, res) => {
+  try {
+    const { question_id } = req.params;
+    await markQuestionReported(question_id);
+    res.status(200).json({ success: true, message: 'Question reported or un-reported successfully!' });
+  } catch (error) {
+    console.error('Error retrieving questions: ', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
