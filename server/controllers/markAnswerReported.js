098c85711de7a3046cdce84f3c8d30253e6c1c52
William Tucker <william.tucker29@gmail.com>
2024-03-22 12:18:46 -0500

    Built API for marking answers as reported. This also lets answers be un-reported. Tested with Postman and GET request to ensure answer removal and addition with the PUT request.

--- server/controllers/markAnswerReported.js
+++ server/controllers/markAnswerReported.js
@@ -1,3 +1,14 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-console */
 const { markAnswerReported } = require('../models');
 
-module.exports = (req, res) => {};
+module.exports = async (req, res) => {
+  try {
+    const { answer_id } = req.params;
+    await markAnswerReported(answer_id);
+    res.status(200).json({ success: true, message: 'Answer reported or un-reported successfully!' });
+  } catch (error) {
+    console.error('Error reporting answers: ', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
