91a6df77b3c2ce41e001751465917cf6429c9697
William Tucker <william.tucker29@gmail.com>
2024-03-22 12:17:22 -0500

    Updated my formattedData function. I am now filtering out any answers that have been reported. This wasn't happening properly before. Tested with Postman using PUT request endpoint for markAnswerReported.

--- server/models/getAnswers.js
+++ server/models/getAnswers.js
@@ -10,6 +10,7 @@ module.exports = async (questionId, page, count) => {
         a.body,
         a.date,
         a.answerer_name,
+        a.answer_reported,
         a.helpfulness,
         json_agg(json_build_object ('id', p.photo_id, 'url', p.photo_url)) AS photos
       FROM
@@ -25,14 +26,16 @@ module.exports = async (questionId, page, count) => {
     };
     await client.connect();
     const result = await client.query(query);
-    const formattedData = await result.rows.map((row) => ({
-      answer_id: row.answer_id,
-      body: row.body,
-      date: row.date,
-      answerer_name: row.answerer_name,
-      helpfulness: row.helpfulness,
-      photos: row.photos,
-    }));
+    const formattedData = await result.rows
+      .filter((row) => !row.answer_reported)
+      .map((row) => ({
+        answer_id: row.answer_id,
+        body: row.body,
+        date: row.date,
+        answerer_name: row.answerer_name,
+        helpfulness: row.helpfulness,
+        photos: row.photos,
+      }));
     const responseObject = {
       question: questionId,
       page,
