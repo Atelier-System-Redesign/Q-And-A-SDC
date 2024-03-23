53b6544f694884a5fcc1a3a321dd4b9f7e9a57fb
William Tucker <william.tucker29@gmail.com>
2024-03-23 13:36:12 -0500

    Removed array methods to format data. Changed SQL query to format data on database side.

--- server/models/getQuestions.js
+++ server/models/getQuestions.js
@@ -4,25 +4,25 @@ const createClient = require('../../db/database.js');
 module.exports = async (productId) => {
   const client = createClient();
   try {
-    const query = 'SELECT * FROM questions WHERE product_id = $1';
+    const query = 'SELECT * FROM questions WHERE product_id = $1 AND NOT reported';
 
     await client.connect();
     const result = await client.query(query, [productId]);
-    const formattedData = result.rows
-      .filter((row) => !row.reported)
-      .map((row) => ({
-        question_id: row.question_id,
-        question_body: row.question_body,
-        question_date: row.question_date,
-        asker_name: row.asker_name,
-        asker_email: row.asker_email,
-        question_helpfulness: row.question_helpfulness,
-        reported: row.reported,
-      }));
+    // const formattedData = result.rows
+    //   .filter((row) => !row.reported)
+    //   .map((row) => ({
+    //     question_id: row.question_id,
+    //     question_body: row.question_body,
+    //     question_date: row.question_date,
+    //     asker_name: row.asker_name,
+    //     asker_email: row.asker_email,
+    //     question_helpfulness: row.question_helpfulness,
+    //     reported: row.reported,
+    //   }));
 
     const responseObject = {
       product_id: productId,
-      results: formattedData,
+      results: result.rows,
     };
 
     return responseObject;
