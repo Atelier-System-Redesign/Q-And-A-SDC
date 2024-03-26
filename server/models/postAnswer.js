f522dc639d3d13b48aef64f757e4911bdfad6905
William Tucker <william.tucker29@gmail.com>
2024-03-26 13:59:04 -0500

    Using Redis as a caching option

--- server/models/postAnswer.js
+++ server/models/postAnswer.js
@@ -1,20 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable camelcase */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
-const createClient = require('../../db/database.js');
+const { createClient } = require('redis');
+const createDBClient = require('../../db/database.js');
 
 module.exports = async (questionId, body, date, name, email, reported, helpful, photos) => {
-  console.log(photos);
-  const client = createClient();
+  const client = createDBClient();
+  const redisClient = createClient();
   try {
-    const query = 'INSERT INTO answers (answer_id, question_id, body, date, answerer_name, answerer_email, answer_reported, helpfulness) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7)';
-    const values = [questionId, body, date, name, email, reported, helpful];
+    const cacheKey = `${questionId}-${body}-${name}`;
+    const answerQuery = 'INSERT INTO answers (answer_id, question_id, body, date, answerer_name, answerer_email, answer_reported, helpfulness) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7)RETURNING answer_id';
+    const answerValues = [questionId, body, date, name, email, reported, helpful];
 
     await client.connect();
-    await client.query(query, values);
-    return { success: true, message: 'Answer posted successfully!' };
+    const { rows: [{ answer_id }] } = await client.query(answerQuery, answerValues);
+    const photoQuery = 'INSERT INTO photos (photo_id, answer_id, photo_url) VALUES (uuid_generate_v4(), $1, $2)';
+    for (const url of photos) {
+      await client.query(photoQuery, [answer_id, url]);
+    }
+
+    const newAnswer = { success: true, message: 'Answer posted successfully!' };
+    await redisClient.connect();
+    await redisClient.set(cacheKey, JSON.stringify(newAnswer));
+    const cachedAnswer = await redisClient.get(cacheKey);
+    return cachedAnswer ? JSON.parse(cachedAnswer) : newAnswer;
   } catch (error) {
     console.error('Error posting question: ', error);
     throw error;
   } finally {
     client.end();
+    redisClient.quit();
   }
 };
--- server/models/postQuestion.js
+++ server/models/postQuestion.js
@@ -1,19 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
-const createClient = require('../../db/database.js');
+const { createClient } = require('redis');
+const createDBClient = require('../../db/database.js');
 
 module.exports = async (productId, body, date, name, email, reported, helpful) => {
-  const client = createClient();
+  const client = createDBClient();
+  const redisClient = createClient();
   try {
+    const cacheKey = `${productId}-${body}-${name}`;
+
     const query = 'INSERT INTO questions (question_id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7)';
     const values = [productId, body, date, name, email, reported, helpful];
-
     await client.connect();
     await client.query(query, values);
-    return { success: true, message: 'Question posted successfully!' };
+
+    const newQuestion = { success: true, message: 'Question posted successfully!' };
+
+    await redisClient.connect();
+    await redisClient.set(cacheKey, JSON.stringify(newQuestion));
+    const cachedQuestion = await redisClient.get(cacheKey);
+    return cachedQuestion ? JSON.parse(cachedQuestion) : newQuestion;
   } catch (error) {
     console.error('Error posting question: ', error);
     throw error;
   } finally {
     client.end();
+    redisClient.quit();
   }
 };
